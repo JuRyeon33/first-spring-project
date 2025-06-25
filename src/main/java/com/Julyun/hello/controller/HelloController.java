aeaaad7a5f7e22ea35dc4f82b999cc5f10d67dfc
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-25 16:22:24 +0900

    first commit

--- src/main/java/com/Julyun/hello/controller/HelloController.java
+++ src/main/java/com/Julyun/hello/controller/HelloController.java
@@ -5,6 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hidden-page")
+    public String hiddenpage() {
+    return "hidden-page";
+
+    }
 
 
     @GetMapping("/test")
